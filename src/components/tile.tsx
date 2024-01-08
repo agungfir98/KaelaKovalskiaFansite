ef4486c138f4bdbae58a35a00a8eac0a15d35b2b
Agung Firmansyah <agungfir98@gmail.com>
2024-01-08 12:31:25 +0700

    sticky parallax components

---
+++ src/components/tile.tsx
@@ -0,0 +1,122 @@
+import React, {
+  ReactNode,
+  createContext,
+  forwardRef,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from 'react'
+import { scrollCtx } from '~/utils/scrollObserver'
+import cn from '~/utils/tailwindMerge'
+
+type WrapperProps = {
+  children: ReactNode
+  numOfPages: number
+}
+
+type Tile = {
+  numOfPages: number
+  currentPage: number
+}
+
+const tileCtx = createContext<Tile>({ numOfPages: 0, currentPage: 0 })
+
+export const TileWrapper = forwardRef<HTMLDivElement, WrapperProps>(
+  ({ children, numOfPages }, ref) => {
+    const refContainer = useRef<HTMLDivElement>(null)
+    useImperativeHandle(ref, () => refContainer.current!, [])
+
+    const { scrollY } = useContext(scrollCtx)
+
+    const screenH = window.innerHeight
+    let currentPage = 0
+
+    const { current: elContainer } = refContainer
+    if (elContainer) {
+      const { clientHeight, offsetTop } = elContainer
+
+      const halfH = screenH / 2
+      const percentY =
+        Math.min(
+          clientHeight + halfH,
+          Math.max(-screenH, scrollY - offsetTop) + halfH,
+        ) / clientHeight
+
+      currentPage = percentY * numOfPages
+    }
+
+    useEffect(() => {
+      const { current: elContainer } = refContainer
+      if (elContainer) {
+        const { clientHeight, offsetTop } = elContainer
+
+        const halfH = screenH / 2
+        const percentY =
+          Math.min(
+            clientHeight + halfH,
+            Math.max(-screenH, scrollY - offsetTop) + halfH,
+          ) / clientHeight
+
+        console.log({ percentY, offsetTop })
+      }
+    }, [screenH, scrollY])
+
+    return (
+      <tileCtx.Provider value={{ currentPage, numOfPages }}>
+        <div
+          ref={refContainer}
+          className={cn('relative')}
+          style={{ height: numOfPages * 100 + 'vh' }}
+        >
+          {children}
+        </div>
+      </tileCtx.Provider>
+    )
+  },
+)
+TileWrapper.displayName = TileWrapper.displayName
+
+export const TileBackground: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  return <div className="absolute h-full w-full">{children}</div>
+}
+
+export const TileContent: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  return <div className="sticky top-0 h-screen w-4/6">{children}</div>
+}
+
+type TileProps = {
+  page: number
+  renderContent: (props: { progress: number }) => any
+}
+
+export const Tile: React.FC<TileProps> = ({ page, renderContent }) => {
+  const { currentPage, numOfPages } = useContext(tileCtx)
+  let progress = Math.max(0, currentPage - page)
+  const refContainer = useRef<HTMLDivElement>(null)
+
+  let opacity = Math.min(1, Math.max(0, progress * 4))
+
+  if (opacity >= 0.8 && page < numOfPages * 4) {
+    opacity = Math.max(0, (1.0 - progress) * 4)
+  }
+
+  return (
+    <div
+      className="absolute top-0"
+      ref={refContainer}
+      style={{
+        pointerEvents: progress <= 0 || progress >= 1 ? 'none' : undefined,
+        opacity,
+      }}
+    >
+      <div className="mx-2 flex h-screen flex-col md:justify-center max-md:pt-20 md:ml-16">
+        {renderContent({ progress })}
+      </div>
+    </div>
+  )
+}
