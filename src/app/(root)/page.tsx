2eedbf6a4c879847d6701f999cdd2c88bb419911
Agung Firmansyah <agungfir98@gmail.com>
2024-01-08 14:06:08 +0700

    main content in sticky parallax finished

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,7 +7,9 @@ import React, {
   useRef,
   useState,
 } from 'react'
+import ImageSelector from '~/components/imageSelector'
 import KaelaRunningText from '~/components/kaelaRunningText'
+import { Tile, TileContent, TileWrapper } from '~/components/tile'
 import useKaelaImage from '~/store/kaelaImage.store'
 import { scrollCtx } from '~/utils/scrollObserver'
 import cn from '~/utils/tailwindMerge'
@@ -17,6 +19,8 @@ const MainPage = () => {
   const mainRef = useRef<HTMLDivElement>(null)
   const runTextRef = useRef<HTMLDivElement>(null)
   const { alt, imageUrl } = useKaelaImage()
+
+  const [numOfPages] = useState(6)
   const [scrollYDecimal, setScrollYDecimal] = useState(0)
 
   const hanldeScrollPercentage = useCallback((decimal: number) => {
@@ -28,16 +32,16 @@ const MainPage = () => {
     const { height } = mainRef.current.getBoundingClientRect()
     document.addEventListener(
       'scroll',
-      () => hanldeScrollPercentage(scrollY / height),
+      () => hanldeScrollPercentage(scrollY / (height / numOfPages)),
       { passive: true },
     )
 
     return () => {
       document.removeEventListener('scroll', () =>
-        hanldeScrollPercentage(scrollY / height),
+        hanldeScrollPercentage(scrollY / (height / numOfPages)),
       )
     }
-  }, [hanldeScrollPercentage, scrollY])
+  }, [hanldeScrollPercentage, numOfPages, scrollY])
 
   useEffect(() => {
     if (!runTextRef.current) return
@@ -46,40 +50,167 @@ const MainPage = () => {
   }, [scrollYDecimal])
 
   return (
-    <div className="text-neutral-600">
-      <div className="max-w-screen h-screen overflow-x-hidden" ref={mainRef}>
-        <div
-          className={cn(
-            'max-w-screen fixed top-[60%] -z-50 h-fit -translate-x-[20%] overflow-x-hidden',
-          )}
-          ref={runTextRef}
-        >
-          <KaelaRunningText />
-        </div>
-        <div
-          className={cn(
-            'fixed left-[50%] top-[20%] -translate-x-[50%] overflow-x-hidden',
-            scrollY > 100 ? 'left-[80%] top-0' : 'top-[20%]',
-            'transition-all duration-700',
-          )}
-        >
-          <div className="relative h-[100vh] w-[100vw] md:w-[70vw]">
-            <Image
-              alt={alt}
-              src={imageUrl}
-              fill
-              objectFit="contain"
-              sizes="100%"
-            />
+    <div className="text-neutral-700">
+      <TileWrapper numOfPages={numOfPages} ref={mainRef}>
+        <div className="max-w-screen sticky top-0 h-screen overflow-hidden">
+          <div className="absolute left-0 h-full w-full">
+            <div className="relative h-full">
+              <div
+                className="absolute left-[50%] top-[60%] h-full translate-x-[-50%]"
+                ref={runTextRef}
+              >
+                <KaelaRunningText />
+              </div>
+              <div
+                className={cn(
+                  'absolute top-0 flex translate-x-[-50%]',
+                  scrollY > 100
+                    ? 'left-[80%]'
+                    : 'left-[50%] translate-y-[20vh] scale-125',
+                  'transition-all duration-700',
+                )}
+              >
+                <div className={cn('relative h-[100vh] w-[80vw]')}>
+                  <Image
+                    fill
+                    src={imageUrl}
+                    alt={alt}
+                    className="object-contain"
+                    sizes="100%"
+                  />
+                </div>
+              </div>
+              <div className="absolute bottom-[10%] right-0">
+                <ImageSelector />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mt-20 h-screen w-screen">
-        <div className="container mx-auto flex h-full max-md:pt-20 md:items-center">
-          <h1 className="w-1/2 px-2 text-2xl font-extrabold uppercase tracking-wide md:text-4xl">
-            The coolest blacksmith from Hololive Indonesia Generation 3rd
-          </h1>
-        </div>
+        <TileContent>
+          <Tile
+            page={1}
+            renderContent={({ progress }) => (
+              <div className="flex text-2xl font-extrabold uppercase md:items-center md:text-5xl lg:text-7xl">
+                <h1 className="z-50">
+                  the coolest blacksmith from hololive indonesia generation 3rd
+                </h1>
+              </div>
+            )}
+          />
+          <Tile
+            page={2}
+            renderContent={({ progress }) => (
+              <div>
+                <h1 className="z-50 text-2xl font-extrabold">Info</h1>
+                <div
+                  id="info"
+                  className="grid grid-cols-[1fr,2fr] text-base md:text-2xl"
+                >
+                  <div>name</div>
+                  <div>
+                    kaela kovalskia <br /> (カエラ・コヴァルスキア)
+                  </div>
+                  <div>birth</div>
+                  <div>30 August</div>
+                  <div>Height</div>
+                  <div>173 cm (5&apos;8&apos;)</div>
+                  <div>zodiac sign</div>
+                  <div>virgo</div>
+                  <div>oshi mark</div>
+                  <div>🔨</div>
+                </div>
+              </div>
+            )}
+          />
+          <Tile
+            page={3}
+            renderContent={() => (
+              <div>
+                <h1 className="text-2xl font-bold">Pemaloe</h1>
+                <div className="md:text-2xl">
+                  <p className="inline bg-white/50">
+                    Pemaloe is official fanbase name of Kaela Kovalskia.
+                    &quot;pemaloe&quot; is an archaic spelling for the
+                    Indonesian word pemalu, which can either mean &quot;hammer
+                    wielder&quot; or &quot;shy person&quot;
+                  </p>
+                  <div className="relative mt-10 h-[30vw] w-[30vw] md:h-[10vw] md:w-[10vw]">
+                    <Image
+                      alt="pimok"
+                      fill
+                      sizes="100%"
+                      src={'/assets/pimok.jpg'}
+                    />
+                    <p className="absolute left-0 top-[100%] text-[.7rem] font-semibold">
+                      *picture of pemaloe
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          />
+          <Tile
+            page={4}
+            renderContent={() => (
+              <div>
+                <h1 className="text-2xl font-bold">Kaeluarga</h1>
+                <div>
+                  <p className="inline bg-white/50 md:text-2xl">
+                    Kaela&apos;s membership are called &quot;Kaeluarga&quot;, a
+                    pun on the Indonesian word &quot;keluarga&quot; means
+                    &quot;family&quot;.
+                    <br />
+                    Kaela and her comunity celebrate two special days per year,
+                    reflecting the number &quot;69&quot;
+                  </p>
+                  <ul className="ml-5 list-outside list-disc md:text-2xl">
+                    <li>
+                      <p className="inline bg-white/50">
+                        9 June is called &quot; Pemaloe Day&quot; in memory of
+                        membership opening
+                      </p>
+                    </li>
+                    <li>
+                      <p className="inline bg-white/50">
+                        6 September for being 6th days of the 9th month
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            )}
+          />
+          <Tile
+            page={5}
+            renderContent={() => (
+              <div>
+                <h1 className="text-2xl font-bold">Galon Chan</h1>
+                <div>
+                  <p className="inline bg-white/50 md:text-2xl">
+                    Galon chan is an official mascot of Kaela Kovalskia.
+                    Introduced on her birthday 30 August 2023 after doing 11
+                    streams for 24 hours.
+                  </p>
+
+                  <div className="relative mt-10 h-[30vw] w-[30vw] md:h-[10vw] md:w-[10vw]">
+                    <Image
+                      alt="pimok"
+                      fill
+                      sizes="100%"
+                      src={'/assets/gachan.gif'}
+                    />
+                    <p className="absolute left-0 top-[100%] text-[.7rem] font-semibold">
+                      *picture of gachan
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          />
+        </TileContent>
+      </TileWrapper>
+      <div className="h-screen bg-red-400">
+        <h1>bluee</h1>
       </div>
     </div>
   )
